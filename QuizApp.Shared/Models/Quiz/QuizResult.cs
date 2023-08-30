8ad021d574ab6bb93fca29bf254088742f26ef1e
TomRingblom <t_ringblom@hotmail.com>
2023-08-30 17:21:00 +0200

    Add Result Component.

---
+++ QuizApp.Shared/Models/Quiz/QuizResult.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizApp.Shared.Models.Quiz
+{
+    public class QuizResult
+    {
+        public QuizQuestionDto? Question { get; set; }
+        public string? Answer { get; set; }
+    }
+}
